1fc2b1d02e3707a82aaae78bb1069eb16ebff731
alejandromsagui <alex.maeso19@gmail.com>
2023-04-20 20:06:42 +0200

    Añadido el carousel y el v-lazy

--- src/views/Vw-Main.vue
+++ src/views/Vw-Main.vue
@@ -1,25 +1,31 @@
 <template>
   <v-container>
-    <v-row class="mt-7" align="center" justify="center">
-  <v-carousel hide-delimiters cycle style="width: 94%">
-    <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover></v-carousel-item>
-    <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/hotel.jpg" cover></v-carousel-item>
-    <v-carousel-item src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" cover></v-carousel-item>
-  </v-carousel>
-</v-row>
+    <v-row align="end" justify="end" sm="6" md="4" d-flex flex-row>
+      <v-col col="10">
+        <v-img cover src="../src/assets/images/GamingZone.jpg" class="mx-auto" :width="550" />
+      </v-col>
+      <v-col col="2" style="width: 75%" class="mr-10">
+        <v-carousel hide-delimiters cycle>
+          <v-carousel-item v-for="(videogame, index) in videogames.slice(0, 4)" :key="index" :src="videogame.thumbnail"
+            cover />
+        </v-carousel>
+      </v-col>
+    </v-row>
 
     <v-row align="center" align-content="center">
       <v-col v-for="videogame in videogamesWithUsers" :key="videogame.id" cols="12" sm="6" md="4">
-        <v-card class="card mx-auto mt-7" :margin="16" elevation="10" max-width="500" height=350>
-          <v-img :src="videogame.thumbnail" height=250 cover></v-img>
-          <v-card-title>{{ videogame.title }}</v-card-title>
-          <v-card-text>
-            <p>Fecha de lanzamiento: {{ videogame.release_date }}</p>
-            <div class="user">
-            <!-- <p><v-icon class="fa-regular fa-user mr-1"></v-icon>{{ videogame.user.username }}</p> -->
-          </div>
-          </v-card-text>
-        </v-card>
+        <v-lazy :options="{ 'threshold': 0.5 }" transition="fade-transition">
+          <v-card class="card mx-auto mt-7" :margin="16" elevation="10" max-width="500" height=350>
+            <v-img :src="videogame.thumbnail" height=250 cover></v-img>
+            <v-card-title>{{ videogame.title }}</v-card-title>
+            <v-card-text>
+              <p>Fecha de lanzamiento: {{ videogame.release_date }}</p>
+              <div class="user">
+                <!-- <p><v-icon class="fa-regular fa-user mr-1"></v-icon>{{ videogame.user.username }}</p> -->
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-lazy>
       </v-col>
     </v-row>
   </v-container>
@@ -76,12 +82,14 @@ const videogamesWithUsers = computed(() => {
 * {
   font-family: "Roboto", sans-serif;
 }
-.user{
+
+.user {
   text-align: right;
 }
-.card:hover{
+
+.card:hover {
   cursor: pointer;
   transform: translateY(-5px);
-  box-shadow: 0 15px 30px rgba(0,0,0,0.3);
+  box-shadow: 0 15px 30px rgba(0, 0, 0, 0.3);
 }
 </style>
