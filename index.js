eb4ad7722468ea389e57e235c8c4545f5967b9ab
alejandromsagui <alex.maeso19@gmail.com>
2023-04-22 19:54:16 +0200

    test

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-import express from 'express';
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+
+//initialise the express package
 const app = express()
 
-// Sirve los archivos de la carpeta `dist`
-app.use(express.static('dist'))
+//use the serve-static package to serve the bundled app files in the dist directory
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-// Redirige todas las rutas a `index.html`, para que Vue Router maneje la navegación
-app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/dist/index.html')
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
 
+//heroku automatically assigns port so leave it to do it's
+//work, don't set a port in the heroku dashboard. while the
+//5000 or whatever number you set will be for your local
+//machine.
 const port = process.env.PORT || 5000
-app.listen(port, () => {
-  console.log(`App is running on port ${port}`)
-})
+app.listen(port)
+console.log(`app is listening on port: ${port}`)
