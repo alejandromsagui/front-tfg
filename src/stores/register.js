c7057c1e1470bf31069d26aca97efb47b3e2cd5c
alejandromsagui <alex.maeso19@gmail.com>
2023-04-24 14:58:13 +0200

    Añadidas mas validaciones pero no funciona del todo si el usuario existe

--- src/stores/register.js
+++ src/stores/register.js
@@ -1,5 +1,54 @@
 import { defineStore } from "pinia";
+import { instance_axios } from '../middlewares/axios';
+import { toast } from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
+import { router } from '../routes';
+
 
 export const useRegister = defineStore({
-    
+    id: 'register',
+    state: () => ({
+        user: null,
+        nicknameExists: false
+    }),
+
+    actions: {
+
+        async getNickname(nickname) {
+            try {
+                const nicknameExists = await instance_axios.get('/getNickname/' + nickname);
+                return nicknameExists.status
+            } catch (error) {
+                console.error(error)
+                return error.response.status
+            }
+        },
+        async signIn(nickname, email, password) {
+
+            const getNickname = await this.getNickname(nickname)
+            
+            if (getNickname === 200) {
+                this.nicknameExists = true
+               
+            }
+            
+            if(getNickname !== 200){
+                nicknameExists = false
+                try {
+                    const user = await instance_axios.post('/register', { nickname, email, password })
+                    this.user = user;
+
+                    toast.success("Te has registrado correctamente", {
+                        autoClose: 2000,
+                        theme: 'colored'
+                    })
+                } catch (error) {
+                    toast.error("Ha ocurrido un error", {
+                        autoClose: 2000,
+                        theme: 'colored'
+                    });
+                }
+            }
+        },
+    }
 })
--- src/views/Vw-Login2.vue
+++ src/views/Vw-Login2.vue
@@ -5,28 +5,29 @@
     <div class="background-register-mobile" v-if="transition == 2 && isMobile"></div>
     <div class="dark-layer"></div>
     <v-container fluid class="fill-height">
-        <v-row align="start" justify="start">
+        <v-row align="center" justify="center">
             <v-col cols="12" sm="8" md="6">
                 <v-window v-model="transition">
                     <v-window-item :value="1">
                         <v-row v-if="!isMobile">
-                            <v-col cols="12" md="8" sm="12" xs="12" class="text-center ml-6">
+                            <v-col cols="12" md="8" sm="12" xs="12" class="text-center mx-auto">
                                 <h2 class="text-white mb-15">
                                     Inicia sesión en Namekians<span style="color:#F80808;">Games</span>
                                 </h2>
                                 <v-form>
                                     <v-text-field label="Usuario" name="usuario" prepend-icon="fa-solid fa-user" type="text"
-                                        class="text-center text-white mr-3" v-model="userLogin.nickname"/>
+                                        class="text-center text-white mr-3" v-model="userLogin.nickname" />
                                     <v-text-field id="contraseña" label="Contraseña" name="Contraseña"
-                                        prepend-icon="fa-solid fa-key" type="password"
-                                        class="text-center text-white mr-3" v-model="userLogin.password"/>
+                                        prepend-icon="fa-solid fa-key" type="password" class="text-center text-white mr-3"
+                                        v-model="userLogin.password" />
                                 </v-form>
                                 <h3 class=" text-center mt-3 text-white"><v-btn variant="plain" href="/"
                                         class="password-recovery">¿Has olvidado tu contraseña?</v-btn></h3>
                                 <h3 class=" text-center mt-3 text-white"><v-btn variant="plain" class="password-recovery"
                                         @click="transition++">Registrate aquí</v-btn></h3>
                                 <div class="text-center mt-3">
-                                    <v-btn rounded color="#F80808" dark class="button mb-6 mt-2" @click="sendData()">Iniciar sesión</v-btn>
+                                    <v-btn rounded color="#F80808" dark class="button mb-6 mt-2" @click="authUser()">Iniciar
+                                        sesión</v-btn>
                                 </div>
                             </v-col>
                         </v-row>
@@ -38,51 +39,65 @@
                                 </h2>
                                 <v-form class="mx-auto">
                                     <v-text-field label="Usuario" name="usuario" prepend-icon="fa-solid fa-user" type="text"
-                                        class="text-center text-white mr-3" v-model="userLogin.nickname"/>
+                                        class="text-center text-white mr-3" v-model="userLogin.nickname" />
                                     <v-text-field id="contraseña" label="Contraseña" name="Contraseña"
-                                        prepend-icon="fa-solid fa-key" class="text-center text-white mr-3" type="password" v-model="userLogin.password"/>
+                                        prepend-icon="fa-solid fa-key" class="text-center text-white mr-3" type="password"
+                                        v-model="userLogin.password" />
                                 </v-form>
                                 <h3 class="text-center mt-3 text-white"><v-btn variant="plain" href="/"
                                         class="password-recovery">¿Has olvidado tu contraseña?</v-btn></h3>
                                 <h3 class="text-center mt-3 text-white"><v-btn variant="plain" class="password-recovery"
                                         @click="transition++">Registrate aquí</v-btn></h3>
                                 <div class="text-center mt-3">
-                                    <v-btn rounded color="#F80808" dark class="button mb-6" @click="sendData()">Iniciar sesión</v-btn>
+                                    <v-btn rounded color="#F80808" dark class="button mb-6" @click="authUser()">Iniciar
+                                        sesión</v-btn>
                                 </div>
                             </v-col>
                         </v-row>
                     </v-window-item>
                     <v-window-item :value="2">
                         <v-row v-if="!isMobile">
-                            <v-col cols="12" md="8" class="ml-6">
-                                    <h2 class="text-white mb-15 text-center">Crea una nueva <span style="color: #F80808;">cuenta</span></h2>
-                                    <v-form>
-                                        <v-text-field label="Usuario" name="Usuario" prepend-icon="fa-solid fa-user"
-                                            type="text" class="text-white"/>
-                                        <v-text-field label="Email" name="email" prepend-icon="fa-solid fa-envelope"
-                                            type="text" class="text-white"/>
-                                        <v-text-field label="Contraseña" name="contraseña" prepend-icon="fa-solid fa-key"
-                                            type="password" class="text-white"/>
+                            <v-col cols="12" md="8" class="text-center mx-auto">
+                                <h2 class="text-white mb-15 text-center">Crea una nueva <span
+                                        style="color: #F80808;">cuenta</span></h2>
+                                <div>
+                                    <v-form ref="form" @submit.prevent="registerUser">
+                                        <v-text-field label="Usuario" prepend-icon="fa-solid fa-user" type="text"
+                                            class="text-white" v-model="userLogin.nickname"
+                                            :rules="[(val) => (val && val.length > 0 || 'Este campo es obligatorio')]" />
+
+                                        <v-text-field label="Email" prepend-icon="fa-solid fa-envelope" type="text"
+                                            class="text-white" v-model="userLogin.email" :rules="[(val) => (val && val.length > 0) || 'Este campo es obligatorio',
+                                                    isValidEmail]" />
+                                        <v-text-field label="Contraseña" prepend-icon="fa-solid fa-key" type="password"
+                                            class="text-white" v-model="userLogin.password" :rules="[(val) => (val && val.length > 0) || 'Este campo es obligatorio',
+                                                (val) => (val && val.length > 5 || 'La contraseña debe ser superior a 5 caracteres')
+                                                ]" />
+                                        <h3 class=" text-center mt-3 text-white"><v-btn variant="plain"
+                                                class="password-recovery" @click="transition--">Inicia sesión aquí</v-btn>
+                                        </h3>
+                                        <div class="text-center mt-3">
+                                            <v-btn rounded color="#F80808" dark class="button mb-6 mt-2"
+                                                type="submit">Registro</v-btn>
+                                        </div>
                                     </v-form>
-                                    <h3 class=" text-center mt-3 text-white"><v-btn variant="plain" class="password-recovery"
-                                        @click="transition--">Inicia sesión aquí</v-btn></h3>
-                                <div class="text-center mt-3">
-                                    <v-btn rounded color="#F80808" dark class="button mb-6 mt-2">Registro</v-btn>
+
                                 </div>
                             </v-col>
                         </v-row>
                         <v-row v-else>
-                            <v-col cols="12" md="8">
-                                    <h2 class="text-white mb-15 text-center">Crea una nueva <span style="color: #F80808;">cuenta</span></h2>
-                                    <v-form>
-                                        <v-text-field label="Usuario" name="Usuario" prepend-icon="fa-solid fa-user"
-                                            type="text" class="text-white"/>
-                                        <v-text-field label="Email" name="email" prepend-icon="fa-solid fa-envelope"
-                                            type="text" class="text-white"/>
-                                        <v-text-field label="Contraseña" name="contraseña" prepend-icon="fa-solid fa-key"
-                                            type="password" class="text-white"/>
-                                    </v-form>
-                                    <h3 class=" text-center mt-3 text-white"><v-btn variant="plain" class="password-recovery"
+                            <v-col cols="12" md="8" class="text-center mx-auto">
+                                <h2 class="text-white mb-15 text-center">Crea una nueva <span
+                                        style="color: #F80808;">cuenta</span></h2>
+                                <v-form>
+                                    <v-text-field label="Usuario" name="Usuario" prepend-icon="fa-solid fa-user" type="text"
+                                        class="text-white" />
+                                    <v-text-field label="Email" name="email" prepend-icon="fa-solid fa-envelope" type="text"
+                                        class="text-white" />
+                                    <v-text-field label="Contraseña" name="contraseña" prepend-icon="fa-solid fa-key"
+                                        type="password" class="text-white" />
+                                </v-form>
+                                <h3 class=" text-center mt-3 text-white"><v-btn variant="plain" class="password-recovery"
                                         @click="transition--">Inicia sesión aquí</v-btn></h3>
                                 <div class="text-center mt-3">
                                     <v-btn rounded color="#F80808" dark class="button mb-6 mt-2">Registro</v-btn>
@@ -99,21 +114,59 @@
 <script setup>
 import { ref, computed, reactive } from "vue"
 import { useLoginStore } from '../stores/login'
+import { useRegister } from "../stores/register";
+import { toast } from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
+import { storeToRefs } from "pinia";
 
 const authStore = useLoginStore();
+const registerStore = useRegister();
+const { nicknameExists } = storeToRefs(registerStore)
+
 const transition = ref(1)
+const form = ref(null);
+
+const userLogin = reactive({
+    nickname: '',
+    email: '',
+    password: ''
+})
 
 const isMobile = computed(() => window.innerWidth <= 960)
 
-const userLogin = reactive({
-  nickname: '',
-  password: ''
-})
-
-const sendData = async () => {
-  await authStore.login(userLogin.nickname, userLogin.password);
+const authUser = async () => {
+    await authStore.login(userLogin.nickname, userLogin.password);
 };
 
+const isValidEmail = (val) => {
+    const emailPattern =
+        /^(?=[a-zA-Z0-9@._%+-]{6,254}$)[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/;
+    return emailPattern.test(val) || "El email no es válido";
+};
+
+const registerUser = async () => {
+
+    let formIsValid = await form.value.validate();
+    
+    if(nicknameExists){
+        toast.error("Ese usuario ya existe", {
+                    autoClose: 2000,
+                    theme: 'colored'
+                });
+    }
+
+    if (formIsValid.valid) {
+        await registerStore.signIn(userLogin.nickname, userLogin.email, userLogin.password)
+        transition.value = 1;
+        form.value.reset();
+    } else {
+        toast.error("Formulario no válido", {
+            autoClose: 2000,
+            theme: 'colored'
+        });
+    }
+
+}
 
 </script>
 
@@ -158,10 +211,12 @@ const sendData = async () => {
     color: #fff;
     font-weight: bold;
 }
+
 .password-recovery:hover {
     color: #F80808;
 }
-.background-mobile{
+
+.background-mobile {
     background-image: url(../assets/images/god-of-war.jpg);
     height: 100%;
     width: 100%;
@@ -170,7 +225,8 @@ const sendData = async () => {
     top: 0;
     background-size: cover;
 }
-.background-register{
+
+.background-register {
     background-image: url(../assets/images/spiderman-register.jpg);
     height: 100%;
     width: 100%;
@@ -180,7 +236,7 @@ const sendData = async () => {
     background-size: cover;
 }
 
-.background-register-mobile{
+.background-register-mobile {
     background-image: url(../assets/images/god-of-war2.jpg);
     height: 100%;
     width: 100%;
@@ -188,6 +244,4 @@ const sendData = async () => {
     position: absolute;
     top: 0;
     background-size: cover;
-}
-
-</style>
+}</style>
