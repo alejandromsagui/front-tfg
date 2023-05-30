01d124745d4d26277c27f2017bf1b5c3f7cb7120
alejandromsagui <alex.maeso19@gmail.com>
2023-05-30 14:36:14 +0200

    despliegue

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,5 +1,5 @@
 
 import io from "socket.io-client"
-export const socket = io("http://127.0.0.1:3000", {
+export const socket = io("https://namekiansgames-backend.herokuapp.com", {
   withCredentials: true
 });
